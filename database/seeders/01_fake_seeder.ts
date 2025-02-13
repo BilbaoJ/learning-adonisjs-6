d5c6500165332485ec927fe859c4399ebc7fc83d
BilbaoJ <jessicabilbao02@gmail.com>
2025-02-13 16:46:45 -0500

    Seeding movies with one to many model factory relationship

--- database/seeders/01_fake_seeder.ts
+++ database/seeders/01_fake_seeder.ts
@@ -3,23 +3,26 @@ import { CineastFactory } from '#database/factories/cineast_factory'
 import { MovieFactory } from '#database/factories/movie_factory'
 import { UserFactory } from '#database/factories/user_factory'
 import MovieStatuses from '#enums/movies_statuses'
+import Cineast from '#models/cineast'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import { DateTime } from 'luxon'
 
 export default class extends BaseSeeder {
   static environment: string[] = ['development', 'testing']
   async run() {
-    await CineastFactory.createMany(10)
+    const cineast = await CineastFactory.createMany(10)
     await UserFactory.with('profile').createMany(5)
-    await this.#createMovies()
+    await this.#createMovies(cineast)
   }
 
-  async #createMovies() {
+  async #createMovies(cineast: Cineast[]) {
     let index = 0
     await MovieFactory.tap((row, { faker }) => {
       const movie = movies[index]
       const released = DateTime.now().set({ year: movie.releaseYear })
       row.statusId = MovieStatuses.RELEASED
+      row.directorId = cineast.at(Math.floor(Math.random() * cineast.length))!.id
+      row.writerId = cineast.at(Math.floor(Math.random() * cineast.length))!.id
       row.title = movie.title
       row.releasedAt = DateTime.fromJSDate(
         faker.date.between({
@@ -30,9 +33,9 @@ export default class extends BaseSeeder {
       index++
     }).createMany(movies.length)
 
-    await MovieFactory.createMany(3)
-    await MovieFactory.apply('released').createMany(2)
-    await MovieFactory.apply('releasingSoon').createMany(2)
-    await MovieFactory.apply('postProduction').createMany(2)
+    await MovieFactory.with('director').with('writer').createMany(3)
+    await MovieFactory.with('director').with('writer').apply('released').createMany(2)
+    await MovieFactory.with('director').with('writer').apply('releasingSoon').createMany(2)
+    await MovieFactory.with('director').with('writer').apply('postProduction').createMany(2)
   }
 }
