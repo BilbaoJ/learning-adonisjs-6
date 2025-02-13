016153d2739615ad31d3db9b1d8315cead5ed2da
BilbaoJ <jessicabilbao02@gmail.com>
2025-02-13 13:58:40 -0500

    Tapping into model factory states

---
+++ database/data/movie.ts
@@ -0,0 +1,154 @@
+export const movies = [
+  {
+    title: '3:10 to Yuma',
+    releaseYear: 1957,
+  },
+  {
+    title: 'The 7th Voyage of Sinbad',
+    releaseYear: 1958,
+  },
+  {
+    title: '12 Angry Men',
+    releaseYear: 1957,
+  },
+  {
+    title: '13 Lakes',
+    releaseYear: 2004,
+  },
+  {
+    title: '20,000 Leagues Under the Sea',
+    releaseYear: 1916,
+  },
+  {
+    title: '42nd Street',
+    releaseYear: 1933,
+  },
+  {
+    title: '2001: A Space Odyssey',
+    releaseYear: 1968,
+  },
+  {
+    title: 'Abbott and Costello Meet Frankenstein',
+    releaseYear: 1948,
+  },
+  {
+    title: 'Ace in the Hole (aka Big Carnival, The)',
+    releaseYear: 1951,
+  },
+  {
+    title: 'Adam’s Rib',
+    releaseYear: 1949,
+  },
+  {
+    title: 'The Adventures of Robin Hood',
+    releaseYear: 1938,
+  },
+  {
+    title: 'The African Queen',
+    releaseYear: 1951,
+  },
+  {
+    title: 'Airplane!',
+    releaseYear: 1980,
+  },
+  {
+    title: 'Alien',
+    releaseYear: 1979,
+  },
+  {
+    title: 'All About Eve',
+    releaseYear: 1950,
+  },
+  {
+    title: 'All My Babies',
+    releaseYear: 1953,
+  },
+  {
+    title: 'All Quiet on the Western Front',
+    releaseYear: 1930,
+  },
+  {
+    title: 'All That Heaven Allows',
+    releaseYear: 1955,
+  },
+  {
+    title: 'All That Jazz',
+    releaseYear: 1979,
+  },
+  {
+    title: 'All the King’s Men',
+    releaseYear: 1949,
+  },
+  {
+    title: 'All the President’s Men',
+    releaseYear: 1976,
+  },
+  {
+    title: 'Allures',
+    releaseYear: 1961,
+  },
+  {
+    title: 'America, America',
+    releaseYear: 1963,
+  },
+  {
+    title: 'American Graffiti',
+    releaseYear: 1973,
+  },
+  {
+    title: 'An American in Paris',
+    releaseYear: 1951,
+  },
+  {
+    title: 'Anatomy of a Murder',
+    releaseYear: 1959,
+  },
+  {
+    title: 'Annie Hall',
+    releaseYear: 1977,
+  },
+  {
+    title: 'Antonia: A Portrait of the Woman',
+    releaseYear: 1974,
+  },
+  {
+    title: 'The Apartment',
+    releaseYear: 1960,
+  },
+  {
+    title: 'Apocalypse Now',
+    releaseYear: 1979,
+  },
+  {
+    title: 'Applause',
+    releaseYear: 1929,
+  },
+  {
+    title: 'The Asphalt Jungle',
+    releaseYear: 1950,
+  },
+  {
+    title: 'Atlantic City',
+    releaseYear: 1980,
+  },
+  {
+    title: 'The Atomic Café',
+    releaseYear: 1982,
+  },
+  {
+    title: 'The Awful Truth',
+    releaseYear: 1937,
+  },
+  {
+    title: 'Baby Face',
+    releaseYear: 1933,
+  },
+  {
+    title: 'Back to the Future',
+    releaseYear: 1985,
+  },
+  {
+    title: 'The Bad and the Beautiful',
+    releaseYear: 1952,
+  },
+]
