20fab8cc7c7f862a8b27f8dbddfe5df0e3c5d3d8
BilbaoJ <jessicabilbao02@gmail.com>
2025-02-14 11:03:33 -0500

    Creating factory relationships from a pool of data

--- database/seeders/01_fake_seeder.ts
+++ database/seeders/01_fake_seeder.ts
@@ -4,20 +4,33 @@ import { MovieFactory } from '#database/factories/movie_factory'
 import { UserFactory } from '#database/factories/user_factory'
 import MovieStatuses from '#enums/movies_statuses'
 import Cineast from '#models/cineast'
+import Movie from '#models/movie'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import { ModelObject } from '@adonisjs/lucid/types/model'
 import { DateTime } from 'luxon'
 
 export default class extends BaseSeeder {
   static environment: string[] = ['development', 'testing']
+  titles: string[] = [
+    'Camera Operator',
+    'Art director',
+    'Hair and Makeup',
+    'Production Manager',
+    'Wardrobe',
+    'Line Producer',
+    'Sound Mixer',
+    'Cinematographer',
+    'Gaffer',
+  ]
   async run() {
-    const cineast = await CineastFactory.createMany(10)
+    const cineast = await CineastFactory.createMany(100)
     await UserFactory.with('profile').createMany(5)
     await this.#createMovies(cineast)
   }
 
   async #createMovies(cineast: Cineast[]) {
     let index = 0
-    await MovieFactory.tap((row, { faker }) => {
+    let movieRecords = await MovieFactory.tap((row, { faker }) => {
       const movie = movies[index]
       const released = DateTime.now().set({ year: movie.releaseYear })
       row.statusId = MovieStatuses.RELEASED
@@ -33,19 +46,57 @@ export default class extends BaseSeeder {
       index++
     }).createMany(movies.length)
 
-    await MovieFactory.with('director')
-      .with('writer')
-      .with('castMembers', 3, (builder) =>
-        builder.pivotAttributes([
-          { character_name: 'Robert', sort_order: 0 },
-          { character_name: 'Joy', sort_order: 1 },
-          { character_name: 'Anna', sort_order: 2 },
-        ])
-      )
-      .with('crewMembers', 5, (builder) => builder.pivotAttributes({ title: 'Camera operator' }))
-      .createMany(3)
-    await MovieFactory.with('director').with('writer').apply('released').createMany(2)
-    await MovieFactory.with('director').with('writer').apply('releasingSoon').createMany(2)
-    await MovieFactory.with('director').with('writer').apply('postProduction').createMany(2)
+    movieRecords = movieRecords.concat(
+      await MovieFactory.with('director').with('writer').createMany(3)
+    )
+
+    movieRecords = movieRecords.concat(
+      await MovieFactory.with('director').with('writer').apply('released').createMany(2)
+    )
+    movieRecords = movieRecords.concat(
+      await MovieFactory.with('director').with('writer').apply('releasingSoon').createMany(2)
+    )
+    movieRecords = movieRecords.concat(
+      await MovieFactory.with('director').with('writer').apply('postProduction').createMany(2)
+    )
+
+    const promises = movieRecords.map(async (movie) => {
+      await this.#attachRandomCastMembers(movie, cineast, 4)
+      return this.#attachRandomCrewMembers(movie, cineast, 3)
+    })
+
+    await Promise.all(promises)
+  }
+
+  async #attachRandomCrewMembers(movie: Movie, cineast: Cineast[], number: number) {
+    const ids = this.#getRandom(cineast, number).map(({ id }) => id)
+    return movie.related('crewMembers').attach(
+      ids.reduce<Record<string, ModelObject>>((obj, id, i) => {
+        obj[id] = {
+          title: this.#getRandom(this.titles, 1)[0],
+          sort_order: i,
+        }
+        return obj
+      }, {})
+    )
+  }
+
+  async #attachRandomCastMembers(movie: Movie, cineast: Cineast[], number: number) {
+    const ids = this.#getRandom(cineast, number).map(({ id }) => id)
+    const records = await CineastFactory.makeStubbedMany(number)
+    return movie.related('castMembers').attach(
+      ids.reduce<Record<string, ModelObject>>((obj, id, i) => {
+        obj[id] = {
+          character_name: records[i].fullName,
+          sort_order: i,
+        }
+        return obj
+      }, {})
+    )
+  }
+
+  #getRandom<T>(array: T[], pluck: number) {
+    const shuffle = array.sort(() => 0.5 - Math.random())
+    return shuffle.slice(0, pluck)
   }
 }
