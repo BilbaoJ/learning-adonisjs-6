32313aa91ce0e6c423e6585a8cea63149ca9c7f0
BilbaoJ <jessicabilbao02@gmail.com>
2025-02-17 14:06:09 -0500

    Validating query string filter values

--- app/services/movie_service.ts
+++ app/services/movie_service.ts
@@ -1,9 +1,11 @@
 import Movie from '#models/movie'
+import { Infer } from '@vinejs/vine/types'
+import { movieFilterValidator } from '#validators/movie'
 
 type MovieSortOption = {
   id: string
   text: string
-  field: keyof Movie
+  field: string
   dir: 'asc' | 'desc' | undefined
 }
 
@@ -13,16 +15,20 @@ export default class MovieService {
     { id: 'title_desc', text: 'Title (desc)', field: 'title', dir: 'desc' },
     { id: 'released_asc', text: 'Released Date (asc)', field: 'releasedAt', dir: 'asc' },
     { id: 'released_desc', text: 'Released Date (desc)', field: 'releasedAt', dir: 'desc' },
+    { id: 'writer_asc', text: 'Writer Name (asc)', field: 'cineasts.last_name', dir: 'asc' },
+    { id: 'writer_desc', text: 'Writer Name (desc)', field: 'cineasts.last_name', dir: 'desc' },
   ]
 
-  static getFiltered(filters: Record<string, any>) {
+  static getFiltered(filters: Infer<typeof movieFilterValidator>) {
     const sort =
       this.sortOptions.find((option) => option.id === filters.sort) || this.sortOptions[0]
 
     return Movie.query()
-      .apply((scope) => scope.released())
       .if(filters.search, (query) => query.whereILike('title', `%${filters.search}%`))
-      .if(filters.status, (query) => query.where('statusId', filters.status))
+      .if(filters.status, (query) => query.where('statusId', filters.status!))
+      .if(['writer_asc', 'writer_desc'].includes(sort.id), (query) => {
+        query.join('cineasts', 'cineasts.id', 'writer_id').select('movies.*')
+      })
       .preload('director')
       .preload('writer')
       .preload('status')
